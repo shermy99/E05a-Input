7adc6c8e749c35f9365e50fe36799d91545ed340
shermy99 <dragonfire1199@gmail.com>
2020-02-11 20:35:19 -0500

    Updates license and accepts user input

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -4,5 +4,9 @@ extends Label
 func _ready():
 	pass
 
+
 func _physics_process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	print(mouse_pos.x)
+	text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
 	pass
